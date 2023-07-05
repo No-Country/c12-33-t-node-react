fe27d849a956965c2c20d00bb477bf3e9d8b8422
MauroMoyano <mauromoyanodev@gmail.com>
2023-07-05 19:09:41 -0300

    Middleware config ok

--- eventbnb/src/middleware.ts
+++ eventbnb/src/middleware.ts
@@ -1,3 +1,23 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request: Request){
+    console.log("Middleware!")
+
+    console.log("method " + request.method)
+    console.log("Url " + request.url)
+
+    const origin = request.headers.get('origin')
+
+    console.log("Header " + request.headers)
+    console.log("origin " + origin)
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: ['/about/:path*', '/login/:path*'],
+}
+
+/*
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
@@ -9,4 +29,4 @@ export function middleware(request: NextRequest) {
 // See "Matching Paths" below to learn more
 export const config = {
     matcher: '/about/:path*',
-}
+}*/
