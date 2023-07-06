40e786eac7364ea7082657e5c0ea2b5b128c9aae
fabiuuu8@gmail.com <fabiuuu8@gmail.com>
2023-07-06 16:02:01 -0300

    create auth flow

--- eventbnb/src/app/layout.jsx
+++ eventbnb/src/app/layout.jsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
+import Provider from '../components/Provider'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,15 +13,15 @@ export const metadata = {
 
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        {children}
-        <Footer />
+        <Provider>
+          <Header />
+          {children}
+          <Footer />
+        </Provider>
       </body>
     </html>
   );
--- eventbnb/src/components/header/UserButton.jsx
+++ eventbnb/src/components/header/UserButton.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+'use client'
+
+import React, {useState} from "react";
 import style from "./Header.module.css";
+import SignIn from "../auth/FormAuth"
 
 export default function UserButton({ showOptions }) {
+
+  const [hidden, setHidden] = useState(false)
+
   return (
     <>
       {showOptions && (
         <div className={style.userButton}>
-          <button>Iniciar Sesión</button>
-          <button>Registrarse</button>
+          <button onClick={() => {setHidden(true)}}>Iniciar Sesión</button>
+          <button onClick={() => {setHidden(true)}}>Registrarse</button>
+          <SignIn hidden={hidden} setHidden={setHidden}></SignIn>
         </div>
       )}
     </>
