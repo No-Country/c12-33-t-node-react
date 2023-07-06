5e82822eb5ebce91b7fa7383c96aa48034fce6ec
Adri-ESM <anggivmorales@gmail.com>
2023-07-06 01:46:57 -0400

    header styles

--- eventbnb/src/components/footer/Footer.jsx
+++ eventbnb/src/components/footer/Footer.jsx
@@ -1,5 +1,23 @@
 import React from "react";
+import style from "./Footer.module.css";
+import Image from "next/image";
 
 export default function Footer() {
-  return <div></div>;
+  return (
+    <div className={style.footerContainer}>
+      <div className={style.footer__logo}>
+        <h3>festbnb</h3>
+      </div>
+      <div className={style.footer__terms}>
+        <h3>Términos y Condiciones</h3>
+      </div>
+      <div className={style.footer__links}>
+        <ul>
+          <li>
+            <a href="#">Contacto:</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
