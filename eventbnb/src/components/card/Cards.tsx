714ceab53084571e2ee0484b541393ae0fa457f4
Adri-ESM <anggivmorales@gmail.com>
2023-07-11 11:07:51 -0400

    styles on header and footer and responsive styles on the all page

--- eventbnb/src/components/card/Cards.tsx
+++ eventbnb/src/components/card/Cards.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import Card from "./CardItem";
+import styles from "./Cards.module.css";
+
 export default function Cards({ list }) {
   return (
-    <div className="inline-flex justify-evenly items-center flex-wrap gap-5 py-12 px-4">
+    <div
+      className={`inline-flex justify-evenly items-center flex-wrap gap-5 py-12 px-4 ${styles.card__container}`}
+    >
       {list.map((card, i) => (
         <Card card={card} key={i} />
       ))}
