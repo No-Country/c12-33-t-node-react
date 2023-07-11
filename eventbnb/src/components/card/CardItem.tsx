d01a79487615b6c69118b614c9c39e8677aa0ae0
Adri-ESM <anggivmorales@gmail.com>
2023-07-11 15:09:46 -0400

    styles on cards with the raiting stars

--- eventbnb/src/components/card/CardItem.tsx
+++ eventbnb/src/components/card/CardItem.tsx
@@ -4,6 +4,7 @@ import { FaStar, FaHeart, FaRegHeart } from "react-icons/fa6";
 import { Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import Image from "next/image";
+import style from "./Cards.module.css";
 
 // Import Swiper styles
 import "swiper/css";
@@ -17,7 +18,28 @@ export default function Card({ card }) {
     setIsFavorite(!isFavorite);
   };
   return (
-    <div className="max-w-[300px] rounded-t-xl bg-white">
+    <div
+      className={`max-w-[300px] rounded-t-xl bg-white group transition-all duration-300 transform hover:scale-105 ${style.group}`}
+    >
+      <div className="flex items-center space-x-0">
+        {Array.from({ length: 5 }).map((_, index) => (
+          <span
+            key={index}
+            className="px-1 flex items-center justify-center"
+            style={{
+              marginTop: "15px",
+              marginBottom: "15px",
+            }}
+          >
+            <FaStar
+              className={`text-black ${
+                index < Math.floor(card.rating) ? "" : "text-white border-black"
+              }`}
+            />
+          </span>
+        ))}
+      </div>
+
       <div className="relative">
         <Swiper
           modules={[Navigation, Pagination]}
@@ -54,7 +76,7 @@ export default function Card({ card }) {
           />
         ) : null}
       </div>
-      <div className="flex flex-row justify-between items-start mt-4">
+      <div className="flex flex-row justify-between items-start mt-4 py-6 px-4">
         <div>
           <p className="text-base text-black font-bold">{card.name}</p>
           <p className="text-sm text-black font-bold">{`${card.city},${card.country}`}</p>
@@ -65,12 +87,6 @@ export default function Card({ card }) {
             <strong>{`S/${card.price}`}</strong> noche
           </p>
         </div>
-        {card.rating ? (
-          <div className="flex items-center justify-center space-x-1 text-black">
-            <FaStar />
-            <p className="text-sm">{card.rating}</p>
-          </div>
-        ) : null}
       </div>
     </div>
   );
--- eventbnb/src/components/filter/Filter.jsx
+++ eventbnb/src/components/filter/Filter.jsx
@@ -1,3 +1,7 @@
 export default function Filter() {
-  return <div className="flex flex-col items-center pt-28 pb-0">Filter</div>;
+  return (
+    <div className="flex flex-col items-center pt-28 pb-0 space-y-4 text-3xl">
+      Filter
+    </div>
+  );
 }
