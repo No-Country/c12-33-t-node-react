8b7f7636a6dc3584b85f2b1b07afc6ee4ab800d8
Adri-ESM <anggivmorales@gmail.com>
2023-07-11 15:44:03 -0400

    styles on cards with the raiting stars

--- eventbnb/src/components/card/Cards.tsx
+++ eventbnb/src/components/card/Cards.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import Card from "./CardItem";
 import styles from "./Cards.module.css";
+import Filter from "../filter/Filter";
 
 export default function Cards({ list }) {
   return (
-    <div
-      className={`inline-flex justify-evenly items-center flex-wrap gap-5 py-12 px-4 ${styles.card__container}`}
-    >
-      {list.map((card, i) => (
-        <Card card={card} key={i} />
-      ))}
+    <div>
+      <div>
+        <Filter />
+      </div>
+      <div
+        className={`inline-flex justify-evenly items-center flex-wrap gap-5 py-12 px-4 ${styles.card__container}`}
+      >
+        {list.map((card, i) => (
+          <Card card={card} key={i} />
+        ))}
+      </div>
     </div>
   );
 }
