8881b385af8d4eb7e3c77bca32089cc3a20b60f8
fabiuuu8@gmail.com <fabiuuu8@gmail.com>
2023-07-14 22:19:56 -0300

    fix(flow): change flow at buttons

--- eventbnb/src/app/become-a-host/[id_user]/photos/page.tsx
+++ eventbnb/src/app/become-a-host/[id_user]/photos/page.tsx
@@ -10,6 +10,7 @@ import BackButton from "@/components/create-halls/BackButton";
 
 import { AiOutlineClose } from 'react-icons/ai'
 import { PiImage, PiImages } from 'react-icons/pi'
+import { useRouter } from 'next/navigation';
 
 const Dropzone = ({ className }) => {
   
@@ -17,6 +18,7 @@ const Dropzone = ({ className }) => {
   const [rejected, setRejected] = useState([])
   const [disabled, setDisabled] = useState(false)
   const {setSalon} = useSalons()
+  // const {router} = useRouter()
 
   const onDrop = useCallback((acceptedFiles, rejectedFiles) => {
     if (acceptedFiles?.length) {
@@ -63,38 +65,42 @@ const Dropzone = ({ className }) => {
     setRejected(files => files.filter(({ file }) => file.name !== name))
   }
 
-  async function action(e) {
-    e.preventDefault()
+  async function action() {
+    // e.preventDefault()
     const endpoint = process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_URL
     
     try {
-        if(files.length < 3) throw new Error("Debe subir al menos 3 imágenes") 
-        for (const file of files) {
-            const formData = new FormData();
-            
-            formData.append('file', file);
-            formData.append("upload_preset", "ml_default");
-            formData.append('api_key', process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY);
+      if(files.length < 3) throw new Error("Debe subir al menos 3 imágenes");
 
-            const response = await fetch(endpoint, {
-              method: 'POST',
-              body: formData
-            });
-            
-            const respData = await response.json();
-            setSalon((prevState) => ({
-                ...prevState,
-                imagenes: [...prevState.imagenes, respData.secure_url]
-            }))
-        }
+      for (const file of files) {
+          const formData = new FormData();
+          
+          formData.append('file', file);
+          formData.append("upload_preset", "ml_default");
+          formData.append('api_key', process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY);
+
+          const response = await fetch(endpoint, {
+            method: 'POST',
+            body: formData
+          });
+          
+          const respData = await response.json();
+          setSalon((prevState) => ({
+              ...prevState,
+              imagenes: [...prevState.imagenes, respData.secure_url]
+          }))
+      }
+
+      // router.push('./structure')
+      window.location.href = "./structure";
     } catch (error) {
-        alert(error.message);
+      alert(error.message);
     }
   }
 
   return (
     <div className='bg-white text-black'>
-        <form onSubmit={action} className='py-12 w-1/2 flex flex-col gap-y-8 mx-auto'>
+        <div  className='py-12 w-1/2 flex flex-col gap-y-8 mx-auto'>
             <h3 className='text-4xl font-semibold -mb-8'>Agregá algunas fotos de tu salón</h3>
             <p className='text-xl text-black/70'>Para comenzar, vas a necesitar 3 fotos. Podés agregar otras más o hacer cambios más adelante.</p>
             <div
@@ -119,85 +125,90 @@ const Dropzone = ({ className }) => {
             </div>
 
       {/* Preview */}
-            <section className='flex flex-col gap-y-12'>
-                
-                {/* Accepted files */}
-                <ul className='flex items-center justify-between gap-x-4 flex-wrap gap-y-12'>
-                {files.map(file => (
-                    <li key={file.name} className={`${file ? 'border-none' : 'border-dashed'} h-80 w-80 border border-black/50 relative rounded-md shadow-lg`}>
-                    {
-                        file ? 
-                        <Image
-                            src={file.preview}
-                            alt={file.name}
-                            width={100}
-                            height={100}
-                            onLoad={() => {
-                            URL.revokeObjectURL(file.preview)
-                            }}
-                            className='h-full w-full rounded-md object-fit'
-                        /> : 
-                        <PiImage></PiImage>
-                    }
-                    <button
-                        type='button'
-                        className='absolute right-3 top-3 flex h-8 w-8 items-center justify-center rounded-full border shadow-xl bg-white transition-colors hover:bg-slate-100'
-                        onClick={() => removeFile(file.name)}
-                    >
-                        <AiOutlineClose className='h-5 w-5 fill-black transition-colors ' />
-                    </button>
-                    <p className='mt-2 text-[12px] font-medium text-stone-500'>
-                        {file.name}
-                    </p>
-                    </li>
-                ))}
-                </ul>
+          <section className='flex flex-col gap-y-12'>
+              
+              {/* Accepted files */}
+              <ul className='flex items-center justify-between gap-x-4 flex-wrap gap-y-12'>
+              {files.map(file => (
+                  <li key={file.name} className={`${file ? 'border-none' : 'border-dashed'} h-80 w-80 border border-black/50 relative rounded-md shadow-lg`}>
+                  {
+                      file ? 
+                      <Image
+                          src={file.preview}
+                          alt={file.name}
+                          width={100}
+                          height={100}
+                          onLoad={() => {
+                          URL.revokeObjectURL(file.preview)
+                          }}
+                          className='h-full w-full rounded-md object-fit'
+                      /> : 
+                      <PiImage></PiImage>
+                  }
+                  <button
+                      type='button'
+                      className='absolute right-3 top-3 flex h-8 w-8 items-center justify-center rounded-full border shadow-xl bg-white transition-colors hover:bg-slate-100'
+                      onClick={() => removeFile(file.name)}
+                  >
+                      <AiOutlineClose className='h-5 w-5 fill-black transition-colors ' />
+                  </button>
+                  <p className='mt-2 text-[12px] font-medium text-stone-500'>
+                      {file.name}
+                  </p>
+                  </li>
+              ))}
+              </ul>
 
-                {/* Rejected Files */}
-                <ul className='flex flex-col'>
-                {rejected.map(({ file, errors }) => (
-                    <li key={file.name} className='flex items-start justify-between'>
-                        <div>
-                            <p className='mt-2 text-sm font-medium text-stone-500'>
-                            {file.name}
-                            </p>
-                            <ul className='text-[12px] text-red-400'>
-                            {errors.map(error => (
-                                <li key={error.code}>{error.message}</li>
-                            ))}
-                            </ul>
-                        </div>
-                        <button
-                            type='button'
-                            className='mt-1 rounded-md border bg-red-700 px-3 py-1 text-[12px] font-semibold uppercase  text-white transition-colors hover:bg-red-600'
-                            onClick={() => removeRejected(file.name)}
-                        >
-                            Eliminar
-                        </button>
-                    </li>
-                ))}
-                </ul>
+              {/* Rejected Files */}
+              <ul className='flex flex-col'>
+              {rejected.map(({ file, errors }) => (
+                  <li key={file.name} className='flex items-start justify-between'>
+                      <div>
+                          <p className='mt-2 text-sm font-medium text-stone-500'>
+                          {file.name}
+                          </p>
+                          <ul className='text-[12px] text-red-400'>
+                          {errors.map(error => (
+                              <li key={error.code}>{error.message}</li>
+                          ))}
+                          </ul>
+                      </div>
+                      <button
+                          type='button'
+                          className='mt-1 rounded-md border bg-red-700 px-3 py-1 text-[12px] font-semibold uppercase  text-white transition-colors hover:bg-red-600'
+                          onClick={() => removeRejected(file.name)}
+                      >
+                          Eliminar
+                      </button>
+                  </li>
+              ))}
+              </ul>
 
-            </section>
-            <div className='w-fit flex gap-x-2'>
-                <button
-                type='submit'
-                className={`${disabled ? 'hover:cursor-not-allowed text-slate-500 bg-gray-200 border border-slate-500' : 'bg-green-700 text-white hover:bg-green-600'} ml-auto mt-1 rounded-md border px-6 py-2 text-lg font-semibold uppercase transition-colors`}
-                >
-                Aceptar
-                </button>
-                <button
-                type='button'
-                onClick={removeAll}
-                className='px-6 py-2 mt-1 rounded-md border bg-red-700 text-lg font-semibold uppercase  text-white transition-colors hover:bg-red-600'
-                >
-                Remover
-                </button>   
-            </div>
-        </form>
+          </section>
+          {/* <div className='w-fit flex gap-x-2'>
+              <button
+              type='submit'
+              className={`${disabled ? 'hover:cursor-not-allowed text-slate-500 bg-gray-200 border border-slate-500' : 'bg-green-700 text-white hover:bg-green-600'} ml-auto mt-1 rounded-md border px-6 py-2 text-lg font-semibold uppercase transition-colors`}
+              >
+              Aceptar
+              </button>
+              <button
+              type='button'
+              onClick={removeAll}
+              className='px-6 py-2 mt-1 rounded-md border bg-red-700 text-lg font-semibold uppercase  text-white transition-colors hover:bg-red-600'
+              >
+              Remover
+              </button>   
+          </div> */}
+        </div>
         <div className="sticky bottom-0 left-0 border-t-2 border-black/20 px-6 py-6 flex items-center justify-between w-full bg-slate-100">
-            <BackButton href="./about-your-place"></BackButton>
-            <ButtonHalls href="./" content="Siguiente" backBtn={true}/>
+          <BackButton href="./about-your-place"></BackButton>
+          <button
+            className={`bg-black/90 hover:bg-black px-6 py-3 rounded-md text-white font-semibold` }
+            onClick={action}
+            >
+            Siguiente
+          </button>
         </div>
     </div>
   )
