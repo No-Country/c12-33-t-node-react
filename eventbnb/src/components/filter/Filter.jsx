7829961462b68f3e2165a55669b25ec5bbeae13e
Adri-ESM <anggivmorales@gmail.com>
2023-07-15 22:20:37 -0400

    filters in carousel and button filter to the others items

--- eventbnb/src/components/filter/Filter.jsx
+++ eventbnb/src/components/filter/Filter.jsx
@@ -116,7 +116,7 @@ export default function Filter() {
       </Swiper>
       <div
         className="absolute left-9 top-10 transform -translate-y-1/2"
-        style={{ marginTop: "130px", marginLeft: "25%" }}
+        style={{ marginTop: "35px", marginLeft: "150px" }}
       >
         <button
           className={`l1ovpqvx cbqov2r atm_1s_glywfm atm_5j_1ssbidh atm_kd_idpfg4 atm_7l_1u9drld atm_9j_tlke0l atm_tl_1gw4zv3 atm_3f_1kgecm9 atm_gi_idpfg4 atm_l8_idpfg4 atm_2a_1u8qnfj atm_2d_1hbpp16 atm_9s_116y0ak atm_vy_1f4h9lt atm_e2_1f4h9lt atm_h_1h6ojuz atm_fc_1h6ojuz atm_uc_x37zl0 atm_mj_1rj4a2z atm_k4-1o5j5ji_1piyxwk atm_9j-1o5j5ji_13gfvf7 atm_7l-1o5j5ji_jajhky atm_3f--xmuf69_1rqy8sm atm_kd-1w3cfyq_glywfm atm_uc-1w3cfyq_x37zl0 atm_70-1w3cfyq_1ys03np atm_3f-e4a3ld_glywfm atm_l8-e4a3ld_idpfg4 atm_gi-e4a3ld_idpfg4 atm_3f-1r4qscq_glywfm atm_kd-6y7yyg_glywfm atm_uc-1w3cfyq-p88qr9_glywfm atm_kd-18zk5v0_glywfm atm_uc-18zk5v0_x37zl0 atm_70-18zk5v0_1ys03np atm_3f-6mgo84_glywfm atm_l8-6mgo84_idpfg4 atm_gi-6mgo84_idpfg4 atm_3f-16p4kaz_glywfm atm_kd-17yx6rv_glywfm atm_uc-18zk5v0-p88qr9_glywfm atm_70-1nos8r-uv4tnr_k3iuej atm_tr-1nos8r-uv4tnr_m1zi52 dir dir-ltr ${style.btnPrev}`}
@@ -126,8 +126,8 @@ export default function Filter() {
       <div
         className="absolute right-9 top-10 transform -translate-y-1/2"
         style={{
-          marginTop: "130px",
-          marginRight: "30%",
+          marginTop: "35px",
+          marginRight: "250px",
         }}
       >
         <button
@@ -135,8 +135,19 @@ export default function Filter() {
           onClick={handleNext}
         />
       </div>
+
       <div className={`flex flex-col items-center ${style.filterButton}`}>
-        <button className={`text-sm ${style.filterButton1}`}>Filtros</button>
+        <button
+          style={{
+            marginTop: "40px",
+            marginRight: "140px",
+            width: "100px",
+            fontSize: "20px",
+          }}
+          className={`text-sm ${style.filterButton1} `}
+        >
+          Filtros
+        </button>
       </div>
     </div>
   );
