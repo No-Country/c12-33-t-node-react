b0f63da949a352a2ebc78c4926eafdc7ce10db01
hectordsol <hectordsol@gmail.com>
2023-07-18 09:48:49 -0300

    Validar usuario en Salones

--- microservicios/Salones/src/middlewares/propietarioValidation.js
+++ microservicios/Salones/src/middlewares/propietarioValidation.js
@@ -1,8 +1,9 @@
-const Usuario = require('../../../Usuarios/src/data');
+const {URL_SERVICIO_USUARIOS} = require("../config/envs");
+const axios = require("axios");
 const { response } = require('../utils');
 module.exports= async(req,res,next)=>{
     const {propietario}=req.body;
-    const usuarioEncontrado = await Usuario.get(propietario);
+    const usuarioEncontrado = await axios.get(URL_SERVICIO_USUARIOS,propietario);
     if(usuarioEncontrado) return next();
     else response(res,400,'Error id usuario propietario no encontrado');
 }
