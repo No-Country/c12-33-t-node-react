a508ade84ee5a3ed880df53fa2a51866ececc8a0
hectordsol <hectordsol@gmail.com>
2023-07-19 00:29:57 -0300

    validar id usuario en Salones con mongoose

--- microservicios/Salones/src/middlewares/propietarioValidation.js
+++ microservicios/Salones/src/middlewares/propietarioValidation.js
@@ -1,9 +1,20 @@
-const {URL_SERVICIO_USUARIOS} = require("../config/envs");
 const axios = require("axios");
 const { response } = require('../utils');
+const ObjectId = require('mongoose').Types.ObjectId;
 module.exports= async(req,res,next)=>{
     const {propietario}=req.body;
-    const usuarioEncontrado = await axios.get(URL_SERVICIO_USUARIOS,propietario);
-    if(usuarioEncontrado) return next();
-    else response(res,400,'Error id usuario propietario no encontrado');
+
+    if(!isValidMongoDBUUID(propietario)) return response(res,400,'Solicituda incorrecta con el ID');
+    const usuarioEncontrado = await axios.get(`http://database:5001/Usuario/${propietario}`);
+    if(usuarioEncontrado.data) return next();
+    else return response(res,404,'Error id usuario propietario no encontrado');
+}
+function isValidMongoDBUUID(id){
+     
+    if(ObjectId.isValid(id)){
+        if((String)(new ObjectId(id)) === id)
+            return true;       
+        return false;
+    }
+    return false;
 }
