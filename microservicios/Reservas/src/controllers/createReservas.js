4564587752bf62d910ac18aeee0e61f56044f4fc
MauroMoyano <mauromoyanodev@gmail.com>
2023-07-19 22:33:32 -0300

    antes de probar el login

--- microservicios/Reservas/src/controllers/createReservas.js
+++ microservicios/Reservas/src/controllers/createReservas.js
@@ -1,8 +1,10 @@
 const Reserva = require('../data');
-const {response} = require('../utils');
+const { response } = require('../utils');
+const { pago } = require('./mercadoPago')
 
 module.exports = async (req, res) => {
     const reserva = req.body;
+    pago(reserva)
     const newReserva = await Reserva.create(reserva);
     response(res,201,newReserva);
 }
---
+++ microservicios/Reservas/src/controllers/mercadoPago/index.js
@@ -0,0 +1,7 @@
+
+
+function pago ({_id, Fecha_inicio_hora, Fecha_fin_hora, cliente, salon}) {
+
+}
+
+module.exports = pago;
