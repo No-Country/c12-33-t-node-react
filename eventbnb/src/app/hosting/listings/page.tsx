e34535678c9715b76efbf8899f670edebf2bfba8
fabiuuu8@gmail.com <fabiuuu8@gmail.com>
2023-07-21 09:50:52 -0300

    create route

---
+++ eventbnb/src/app/hosting/listings/page.tsx
@@ -0,0 +1,11 @@
+import MySalons from "@/components/hosting/MySalons";
+
+const page = () => {
+    return(
+        <div className="w-3/4 py-20 mx-auto">
+            <MySalons></MySalons>
+        </div>
+    )
+}
+
+export default page;
