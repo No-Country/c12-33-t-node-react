9d3b51a0ceeca8c7c05775cf82a10b2f41982f03
Mauro Moyano <mauromoyanodev@gmail.com>
2023-07-24 15:22:44 -0300

    Update index.js

--- microservicios/Usuarios/src/middlewares/index.js
+++ microservicios/Usuarios/src/middlewares/index.js
@@ -1,9 +1,5 @@
 module.exports={
     usuarioValidation: require('./usuarioValidation'),
-<<<<<<< HEAD
-    verificarToken: require('./verificarToken'),
-=======
     idValidation: require('./idValidation'),
->>>>>>> e270dd3cd674567f589c86f8e544c96d48b3e6a4
     authMiddlewares  : require('./authMiddlewares'),
 }
