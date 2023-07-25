33816056d9d983e7e27d06785f01948896ad9fbe
Adri-ESM <anggivmorales@gmail.com>
2023-07-24 21:51:54 -0400

    filtering

--- eventbnb/src/components/filter/filterModal/FilterModal.jsx
+++ eventbnb/src/components/filter/filterModal/FilterModal.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useEffect, useContext } from "react";
-import Card from "@/components/card/CardItem";
+import CardItem from "@/components/card/CardItem";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import Price from "./selection/PriceRange";
@@ -8,15 +8,11 @@ import Services from "./selection/AccesicibiliidadFilters";
 import style from "../Filter.module.css";
 import { FilterContext, FilterProvider } from "@/context/FilterProvider";
 import { handleAccesibility, handlePriceRange } from "./handlersModalFilters";
-import showSalonButton from "./showSalonButton";
 
 export let filteredCards = []; // Variable exportada
 export let setFilteredCards = () => {}; // Variable exportada
 
 export default function FilterModal({ list }) {
-  // Call the imported handleAccesibility function for filtering
-  handleCheckboxChange(isSelectedServices, setFilteredCards, list);
-
   const { filteredCards, setFilteredCards } = useContext(FilterContext);
   const [isPriceFiltered, setIsPriceFiltered] = useState(false);
   const [showModal, setShowModal] = useState(false);
@@ -28,9 +24,6 @@ export default function FilterModal({ list }) {
   const [isSelectedServices, setIsSelectedServices] = useState(false);
   const [selectedServices, setSelectedServices] = useState(false);
   const [selectedOptions, setSelectedOptions] = useState([]);
-  const [showSalon, setShowSalon] = useState(false);
-  const [searchResults, setSearchResults] = useState([]);
-  const [serviceKeys, setServiceKeys] = useState([]);
 
   const router = useRouter();
   const url = process.env.MICROSERVICIOS;
@@ -70,8 +63,6 @@ export default function FilterModal({ list }) {
     console.log("EN EL handlePrice");
     setSelectedPrice(!selectedPrice);
     fetchFilteredSalones();
-
-    handlePriceRange(isPriceFiltered, setFilteredCards, list);
   };
 
   //---------  FILTROS DE SERVICIOS ----------------
@@ -88,18 +79,6 @@ export default function FilterModal({ list }) {
     setSelectedOptions([]);
 
     setFilteredCards(list);
-  };
-
-  const handleShowSalon = () => {
-    handlePriceRange(isPriceFiltered, setFilteredCards, list);
-    const filteredSalones = filteredCards.filter((salon) =>
-      serviceKeys.every(
-        (serviceKey) => !isSelectedServices[serviceKey] || salon[serviceKey]
-      )
-    );
-    setSearchResults(filteredSalones);
-    setShowSalon(true);
-    router.push("/");
   };
 
   return (
@@ -167,7 +146,7 @@ export default function FilterModal({ list }) {
                   </div>
                   <div className="bg-white">
                     <button
-                      onClick={handleShowSalon}
+                      onClick={toggleModal}
                       className="text-2xl border border-gray-200 py-2 px-4 rounded-lg hover:shadow-md hover:font-bold"
                     >
                       Mostrar <span>#</span> salones
@@ -178,13 +157,11 @@ export default function FilterModal({ list }) {
             </div>
           )}
         </div>
-
-        {searchResults.map((salon) =>
-          !selectedPrice ||
-          (salon.price >= minPrice && salon.price <= maxPrice) ? (
-            <Card card={salon} key={salon.id} />
-          ) : null
-        )}
+        <div>
+          {filteredCards.map((card) => (
+            <CardItem key={card.id} card={card} />
+          ))}
+        </div>
       </div>
     </FilterProvider>
   );
