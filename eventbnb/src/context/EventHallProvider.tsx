aa4c83cbd83ca118eb630d6f28e9eab4c4d01c03
wilmer-victus <wilmer.r2004@gmail.com>
2023-07-26 15:49:38 -0500

    servicios

--- eventbnb/src/context/EventHallProvider.tsx
+++ eventbnb/src/context/EventHallProvider.tsx
@@ -3,10 +3,14 @@ import { IEventHall } from "@/interfaces/event-hall.interface";
 import EventHallService from "@/services/event-hall.service";
 import { createContext, useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import servicesData, {
+  IServicesData,
+} from "@/components/Hall/Infomation/servicesData";
 
 // interface
 export interface IEventHallProvider {
   eventHall: IEventHall | null;
+  services: IServicesData[] | null;
   getEventHall: () => Promise<void>;
 }
 
@@ -20,6 +24,7 @@ export const EventHallContext = createContext<IEventHallProvider | null>(null);
 
 export const EventHallProvider = ({ children, id }: IProps) => {
   const [eventHall, setEventHall] = useState<IEventHall | null>(null);
+  const [services, setServices] = useState<IServicesData[] | null>(null);
   const router = useRouter();
 
   const getData = async () => {
@@ -35,12 +40,59 @@ export const EventHallProvider = ({ children, id }: IProps) => {
     setEventHall(data);
   };
 
+  const getServices = async () => {
+    let services: IServicesData[] = servicesData.filter((service) => {
+      if (
+        eventHall?.baño_accesibilidad &&
+        service.type === "baño_accesibilidad"
+      ) {
+        return true;
+      } else if (eventHall?.accesibilidad && service.type === "accesibilidad") {
+        return true;
+      } else if (
+        eventHall?.estacionamiento &&
+        service.type === "estacionamiento"
+      ) {
+        return true;
+      } else if (eventHall?.catering && service.type === "catering") {
+        return true;
+      } else if (eventHall?.mesas_sillas && service.type === "mesas_sillas") {
+        return true;
+      } else if (eventHall?.luces && service.type === "luces") {
+        return true;
+      } else if (eventHall?.sonido && service.type === "sonido") {
+        return true;
+      } else if (eventHall?.fotografia && service.type === "fotografia") {
+        return true;
+      } else if (eventHall?.decoracion && service.type === "decoracion") {
+        return true;
+      } else if (eventHall?.pileta && service.type === "pileta") {
+        return true;
+      } else if (eventHall?.wifi && service.type === "wifi") {
+        return true;
+      } else if (eventHall?.cocina && service.type === "cocina") {
+        return true;
+      } else if (eventHall?.escenario && service.type === "escenario") {
+        return true;
+      }
+      return false;
+    });
+
+    setServices(services);
+  };
+
   useEffect(() => {
     getData();
   }, []);
 
+  useEffect(() => {
+    eventHall && getServices();
+  }, [eventHall]);
+
   return (
-    <EventHallContext.Provider value={{ eventHall, getEventHall: getData }}>
+    <EventHallContext.Provider
+      value={{ eventHall, services, getEventHall: getData }}
+    >
       {children}
     </EventHallContext.Provider>
   );
