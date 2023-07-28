c09cef09014cbb31d963fca0e883201eca8f87d5
vlmnst <vale-mansueto@hotmail.com>
2023-07-28 00:40:37 -0300

    separando archivos

--- eventbnb/src/app/api/auth/[...nextauth]/route.ts
+++ eventbnb/src/app/api/auth/[...nextauth]/route.ts
@@ -1,20 +1,27 @@
+// import NextAuth from "next-auth/next";
+// // import { OAuthConfig } from "next-auth/providers";
+// import GoogleProvider, { GoogleProfile } from "next-auth/providers/google";
+
+// export const authOptions = {
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID as string,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+//     }),
+
+//     // FacebookProvider({
+//     //   clientId: process.env.FACEBOOK_CLIENT_ID as string,
+//     //   clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string
+//     // })
+//   ],
+// };
+
+// const handler = NextAuth(authOptions);
+
+// export { handler as GET, handler as POST };
+
+import { authOptions } from "@/app/api/utils/authOptions";
 import NextAuth from "next-auth/next";
-// import { OAuthConfig } from "next-auth/providers";
-import GoogleProvider, { GoogleProfile } from "next-auth/providers/google";
-
-export const authOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-
-    // FacebookProvider({
-    //   clientId: process.env.FACEBOOK_CLIENT_ID as string,
-    //   clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string
-    // })
-  ],
-};
 
 const handler = NextAuth(authOptions);
 
