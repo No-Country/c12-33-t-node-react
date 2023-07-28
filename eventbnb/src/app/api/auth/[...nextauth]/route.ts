5ac739fa0230f2865429b5c7d65b5bb3b6e65493
vlmnst <vale-mansueto@hotmail.com>
2023-07-28 00:31:05 -0300

    dejo como estaba

--- eventbnb/src/app/api/auth/[...nextauth]/route.ts
+++ eventbnb/src/app/api/auth/[...nextauth]/route.ts
@@ -1,43 +1,21 @@
-// import NextAuth from "next-auth";
-// // import { OAuthConfig } from "next-auth/providers";
-// import GoogleProvider, { GoogleProfile } from "next-auth/providers/google";
+import NextAuth from "next-auth";
+// import { OAuthConfig } from "next-auth/providers";
+import GoogleProvider, { GoogleProfile } from "next-auth/providers/google";
 
-// export const authOptions = {
-//   providers: [
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_CLIENT_ID as string,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-//     }),
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
 
-//     // FacebookProvider({
-//     //   clientId: process.env.FACEBOOK_CLIENT_ID as string,
-//     //   clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string
-//     // })
-//   ],
-// };
+    // FacebookProvider({
+    //   clientId: process.env.FACEBOOK_CLIENT_ID as string,
+    //   clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string
+    // })
+  ],
+};
 
-// export default NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 
-import NextAuth from 'next-auth/next';
-import GoogleProvider from 'next-auth/providers/google';
-
-
-export default NextAuth({
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
-        }),
-    ], 
-    session: {
-        maxAge: 30, // Duración de la sesión: 30 segundos
-    },
-    cookies: {
-      sessionToken: {
-        name: `next-auth.session-token`,
-        options: {
-          maxAge: 30, // Duración de la sesión: 30 segundos
-        },
-      },
-    }
-});
+export { handler as GET, handler as POST };
