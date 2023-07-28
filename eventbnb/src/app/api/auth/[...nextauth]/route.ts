003730fa35684858f34473b1b72b1e1f076bbca8
vlmnst <vale-mansueto@hotmail.com>
2023-07-28 00:34:37 -0300

    modifico import

--- eventbnb/src/app/api/auth/[...nextauth]/route.ts
+++ eventbnb/src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
-import NextAuth from "next-auth";
+import NextAuth from "next-auth/next";
 // import { OAuthConfig } from "next-auth/providers";
 import GoogleProvider, { GoogleProfile } from "next-auth/providers/google";
 
